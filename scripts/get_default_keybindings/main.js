4759a351f076c921229906369fb20cec3215a345
Code Bling <codeblingx@gmail.com>
2022-04-22 03:41:54 -0400

    Don't catch errors and force exit, let the errors bubble up and we'll exit anyway

--- scripts/get_default_keybindings/main.js
+++ scripts/get_default_keybindings/main.js
@@ -2,29 +2,24 @@ const path = require('path');
 const { runTests } = require('@vscode/test-electron');
 
 async function main() {
-    try {
-        // Make two empty directories to use to launch VS Code with the cleanest possible profile.
-        const emptyDir1 = path.resolve(__dirname, 'empty1');
-        const emptyDir2 = path.resolve(__dirname, 'empty2');
+    // Make two empty directories to use to launch VS Code with the cleanest possible profile.
+    const emptyDir1 = path.resolve(__dirname, 'empty1');
+    const emptyDir2 = path.resolve(__dirname, 'empty2');
 
-        // Path to the script that retrieves and saves the default keybindings JSON to a file.
-        const scriptPath = path.resolve(__dirname, 'main_impl.js');
+    // Path to the script that retrieves and saves the default keybindings JSON to a file.
+    const scriptPath = path.resolve(__dirname, 'main_impl.js');
 
-        // Download VS Code, unzip it, launch it without extensions, and run the script.
-        await runTests({
-            extensionDevelopmentPath: __dirname,
-            extensionTestsPath: scriptPath,
-            launchArgs: [
-                '--extensions-dir',
-                emptyDir1,
-                '--user-data-dir',
-                emptyDir2
-            ]
-        });
-    } catch (err) {
-        console.error('Failed to run');
-        process.exit(1);
-    }
+    // Download VS Code, unzip it, launch it without extensions, and run the script.
+    await runTests({
+        extensionDevelopmentPath: __dirname,
+        extensionTestsPath: scriptPath,
+        launchArgs: [
+            '--extensions-dir',
+            emptyDir1,
+            '--user-data-dir',
+            emptyDir2
+        ]
+    });
 }
 
 main();
